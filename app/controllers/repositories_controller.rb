8673a80d8749a24675e2eb23ea64e9f229d587e4
tomcat1911 <maslovtimofey@gmail.com>
2018-09-18 23:02:11 +0500

    Basic github top contributors fetch

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,4 +1,25 @@
 class RepositoriesController < ApplicationController
   def index; end
-  def create; end
+
+  def create
+    response = Net::HTTP.get(build_url)
+    response = JSON.parse(response)
+    @contributors = response.last(3)
+    @contributors.map! do |contributor|
+      author = contributor['author']
+      {
+        login: author['login'],
+        avatar_url: author['avatar_url'],
+        url: author['html_url']
+      }
+    end
+  end
+
+  private
+
+  def build_url
+    owner, repo = params[:url].split('/').last(2)
+    url = "https://api.github.com/repos/#{owner}/#{repo}/stats/contributors"
+    URI url
+  end
 end
