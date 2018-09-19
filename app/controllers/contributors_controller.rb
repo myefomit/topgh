85f5df00341ab9a5c877c621f40b7d646c2443a0
tomcat1911 <maslovtimofey@gmail.com>
2018-09-19 22:12:02 +0500

    Refactor to github adapter

--- app/controllers/contributors_controller.rb
+++ app/controllers/contributors_controller.rb
@@ -2,18 +2,8 @@ class ContributorsController < ApplicationController
   def index; end
 
   def create
-    response = Net::HTTP.get(build_url)
-    response = JSON.parse(response)
-    @contributors = response.last(3)
-    @contributors.map! do |contributor|
-      author = contributor['author']
-      contributor_params = ({
-        login: author['login'],
-        avatar_url: author['avatar_url'],
-        url: author['html_url']
-      })
-      build_contributor contributor_params
-    end
+    @contributors = GithubAdapter::Repo.new(url: params[:url]).contributors
+    @contributors.map! { |contributor| build_contributor(contributor) }
   end
 
   private
@@ -21,11 +11,5 @@ class ContributorsController < ApplicationController
   def build_contributor(contributor)
     Contributor.find_by(login: contributor[:login]) ||
     Contributor.create(contributor)
-  end
-
-  def build_url
-    owner, repo = params[:url].split('/').last(2)
-    url = "https://api.github.com/repos/#{owner}/#{repo}/stats/contributors"
-    URI url
   end
 end
---
+++ app/services/github_adapter.rb
@@ -0,0 +1,45 @@
+module GithubAdapter
+  class Repo
+
+    API_BASE_URL = 'https://api.github.com/repos/'
+    CONTRIBUTORS_NUMBER = 3
+
+    def initialize(url:)
+      @url = url
+    end
+
+    def contributors
+      contributors = request_contributors
+
+      # GitHub API v3 returns '{}' for every new repo requested
+      # but it works well with further requests
+      contributors = request_contributors if contributors == {}
+      parse_contributors contributors
+    end
+
+    private
+
+    def request_contributors
+      response = Net::HTTP.get(build_api_url)
+      response = JSON.parse(response)
+    end
+
+    def parse_contributors(response)
+      contributors = response.last(CONTRIBUTORS_NUMBER)
+      contributors.map do |contributor|
+        author = contributor['author']
+        {
+          login: author['login'],
+          avatar_url: author['avatar_url'],
+          url: author['html_url']
+        }
+      end
+    end
+
+    def build_api_url
+      owner, repo = @url.split('/').last(2)
+      url = "#{API_BASE_URL}#{owner}/#{repo}/stats/contributors"
+      URI url
+    end
+  end
+end
